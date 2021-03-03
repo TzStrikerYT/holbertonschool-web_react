47498b6c9a80d75e7e75f33eacff43e295c00e3e
TzStrikerYT <maicol.asus@gmail.com>
2021-03-03 13:33:14 -0500

    [add] task 1 to test checks

---
+++ 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+/* Styles for row */
+
+const rowStyle = {
+    backgroundColor: "#f5f5f5ab" /*bg-color */
+}
+
+/* Styles for header*/
+const headerRowStyle = {
+    backgroundColor: "#f5f5f5ab" /* bg-color */
+}
+
+function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+    let clRow;
+
+    if (isHeader === true) {
+        if (textSecondCell) {
+            clRow = <><th>{textFirstCell}</th><th>{textSecondCell}</th></>;
+        } else {
+            clRow = <><th colSpan="2">{textFirstCell}</th></>;
+        }
+    } else if (isHeader === false) {
+        clRow = <><td>{textFirstCell}</td><td>{textSecondCell}</td></>;
+    }
+
+    /* Styles application */
+
+    let headerStyle;
+
+    /* if header exist apply styles to row else apply header only in the header */
+    headerStyle = isHeader ? headerStyle : headerRowStyle
+
+    return <tr style={headerStyle}>{clRow}</tr>;
+}
+
+CourseListRow.defaultProps = {
+    isHeader: false,
+    textSecondCell: null,
+};
+
+CourseListRow.propTypes = {
+    isHeader: PropTypes.bool,
+    textFirstCell: PropTypes.string.isRequired,
+    textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+};
+
+export default CourseListRow;
